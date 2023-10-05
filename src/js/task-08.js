24b30b928277c7c7c21eb379b33b4296add71a50
Maxym <103531640+MfD0@users.noreply.github.com>
2023-10-05 14:40:53 +0300

    Додає скрипт 8 завдання

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,23 @@
 
+const loginForm = document.querySelector('.login-form');
+
+loginForm.addEventListener('submit', function (event) {
+  event.preventDefault();
+
+  const emailInput = loginForm.elements.email;
+  const passwordInput = loginForm.elements.password;
+
+  if (emailInput.value === '' || passwordInput.value === '') {
+    alert('Заповніть поля форми !!!');
+    return;
+  }
+
+  const Data = {
+    email: emailInput.value,
+    password: passwordInput.value,
+  };
+
+  console.log(Data);
+
+  loginForm.reset();
+})
