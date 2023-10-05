28691d4b66d1bea786860807e817cc92a6cbfb8b
Maxym <103531640+MfD0@users.noreply.github.com>
2023-10-05 14:50:33 +0300

    Скрипт для 9 завдання

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -3,3 +3,17 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const body = document.body;
+const ChangeBGColorBotton = document.querySelector('.change-color');
+const NameBGColorLine = document.querySelector('.color');
+
+ChangeBGColorBotton.addEventListener('click', () => {
+  
+  const NewColor = getRandomHexColor();
+  const NewColorForBotton = getRandomHexColor();
+
+  ChangeBGColorBotton.style.backgroundColor = NewColorForBotton;
+  NameBGColorLine.textContent = NewColor;
+  body.style.backgroundColor = NewColor;
+})
